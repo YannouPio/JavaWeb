4c14f0687959c5245d5820eb1ff172eb78c7837c
zhanglingxi <zhanglingxi44@gmail.com>
2023-11-22 22:55:00 +0800

    更新首页

---
+++ code/web/vue/demo-vue/src/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <div class="logo_box">
+      <el-image src="@/assets/image/bilibili.png" fit="fill"/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
